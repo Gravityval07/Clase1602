81480328cf5d62eaf06fbb2156fe7b32feb054aa
Juanchoscor <juan.del01@usa.edu.co>
2024-02-16 09:12:30 -0500

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public DataJSON misDatos;
+    public InputField textoJugador;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string filePat = Application.streamingAssetsPath + "/" + "data1.json";
+
+        if (File.Exists(filePat))
+        {
+            string s = File.ReadAllText(filePat);
+            misDatos = JsonUtility.FromJson<DataJSON>(s);
+            Debug.Log(s);
+            Debug.Log(misDatos);
+            Debug.Log(misDatos.nombre_juego);
+            Debug.Log(misDatos.nombre_jugador1);
+            s = JsonUtility.ToJson(misDatos, true);
+            Debug.Log(s);
+            File.WriteAllText(filePat, s);
+        }
+        cargaDatos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void cargaDatos()
+    {
+        GameObject.Find("Titulo").GetComponent<Text>().text=misDatos.nombre_juego;
+        GameObject.Find("Nombre").GetComponent<Text>().text = misDatos.nombre_jugador1;
+    }
+    public void guardaDatos()
+    {
+        misDatos.nombre_jugador1 = textoJugador.text;
+        string filePat = Application.streamingAssetsPath + "/" + "data1.json";
+        string s = JsonUtility.ToJson(misDatos, true);
+        File.WriteAllText(filePat, s);
+    }
+}
---
+++ Assets/Scripts/datos.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Serialization;
+
+[System.Serializable]
+public class DataJSON : System.Object
+{
+
+    [SerializeField]
+    public string nombre_juego;
+    [SerializeField]
+    public string nombre_jugador1;
+    [SerializeField]
+    public int nivel1;
+    [SerializeField]
+    public int salud;
+    [SerializeField]
+    public string correo;
+
+    public DataJSON()
+    {
+    }
+
+}
